89c89c114574a403af7ad61a4e64bfe85ef8b159
gchiramattel1 <george_chiramattel@intuit.com>
2018-06-26 12:30:25 -0700

    Introduction to loops and breaks

---
+++ 03_loops/README.md
@@ -0,0 +1,9 @@
+## Loops  breaks
+
+
+```python
+student_names = ['Ann', 'Liza', 'George']
+
+for name in student_names:
+    print(f"Name ->{name}")
+```
---
+++ 03_loops/loops.py
@@ -0,0 +1,19 @@
+# example of for loops
+
+student_names = ['Ann', 'Liza', 'George']
+
+for name in student_names:
+    print(f"Name ->{name}")
+
+print("\n----------\n")
+
+# use the range function to specify a range to iterate on
+
+for index in range(10):
+    print(index)
+
+print("\n----------\n")
+
+for index in range(0, 21, 2):       # starting at 0 till < 21 and incr by 2
+    print(index)
+
